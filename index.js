bdd1249d94bff080210d29ef46be15995c252ef9
Oliver Turner <oliver.turner@ticketmaster.co.uk>
2015-06-19 12:33:36 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+module.exports = function (babel) {
+  var t = babel.types;
+
+  return new babel.Transformer("ignore-local-styles", {
+    CallExpression(node, parent) {
+      var isRequire = t.isIdentifier(node.callee, {name: "require"});
+      if (!isRequire) return;
+      if (node.arguments.length !== 1) return;
+
+      var argument = node.arguments[0];
+
+      if (argument.type !== "Literal" || typeof argument.value !== "string") return;
+      var ext = argument.value.split('.').pop();
+      if (ext === 'scss' || ext === 'css') {
+        this.dangerouslyRemove();
+      }
+    }
+  });
+};
